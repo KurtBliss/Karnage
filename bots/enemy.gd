964310f27a705ef1f1d00c2ef33b9bb6b31675b1
KurtBliss <kurtbliss3@gmail.com>
2020-11-10 22:43:45 -0500

    Update enemy.gd

--- bots/enemy.gd
+++ bots/enemy.gd
@@ -4,18 +4,24 @@ enum {IDLE, USE_PATHFING, PLAYER_IN_RANGE}
 var state_ = USE_PATHFING
 
 func _ready():
-	set_physics_state("state_in_range")
+	set_physics_state("state_idle")
 
 func state_idle(delta):
 	if not get_player():
 		return
 	
 	var is_not_hidden = get_player_visibility()
-	var is_visible = get_player_direction()<45
-		
-	if is_not_hidden and is_visible:
-		set_physics_state("state_chase")
 	
+#	print(rad2deg(get_player_direction()))
+	var dir = get_player_direction()
+	
+	print(Vector3(rad2deg(dir.x), rad2deg(dir.y), rad2deg(dir.z)))
+	
+#	var is_visible = get_player_direction() < 45
+#
+#	if is_not_hidden and is_visible:
+#		set_physics_state("state_chase")
+#
 
 func state_chase(delta):
 	if not get_player():
@@ -25,8 +31,10 @@ func state_chase(delta):
 	var is_not_hidden = get_player_visibility()
 	
 	if is_not_close and is_not_hidden:
+		print("28")
 		do_chase_player()
 	elif is_not_close:
+		print("31")		
 		set_path_to_player()
 		set_physics_state("state_chase_path")
 
@@ -39,7 +47,8 @@ func state_chase_path(delta):
 	
 	if is_running:
 		if is_not_hidden:
-			set_physics_state("is_not_hidden")
-		pass
+			set_physics_state("state_chase")
+		else:
+			pass #player allready moved... ^
 	
 
