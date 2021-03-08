2734696962c73129df179a41990821d1a17bc61d
KurtBliss <kurtbliss3@gmail.com>
2021-03-08 17:09:04 -0500

    Update pickup.gd

--- entities/pickups/pickup.gd
+++ entities/pickups/pickup.gd
@@ -2,26 +2,25 @@ extends Area
 
 var curbody = null
 
+export(NodePath) onready var parrent_path
+onready var parrent = get_node(parrent_path)
+
 func _ready():
 	set_process(false)
 
 func _process(_delta):
 	if not Input.is_action_just_pressed("interact"):
 		return
+		
 	var weapons = curbody.Weapons
-	if !weapons.has_weapon(weapon_pickup_name):
-		weapons.add_weapon(weapon_pickup_name)
-		weapons.switch_weapon_by_name(weapon_pickup_name)
+	if true:#not weapons.has_weapon():
+		weapons.add_weapon(parrent.weapon)
 		queue_free()
 
 func _on_PistolPickup_body_entered(body):
-	print("player touched pistol pickup")
 	if body == Master.Player:
 		curbody = body
 		set_process(true)
-	elif body.is_in_group("enemy"):
-		if get_parent().velocity.length() > 0:
-			body.do_damage(3, null)
 
 func _on_Area_body_exited(body):
 	if body == curbody:
