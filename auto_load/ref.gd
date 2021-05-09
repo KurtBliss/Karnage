3d2eb2b13d00510d63453222c09bbac61b416256
KurtBliss <kurtbliss3@gmail.com>
2021-05-08 20:52:39 -0400

    Update ref.gd

--- auto_load/ref.gd
+++ auto_load/ref.gd
@@ -1,5 +1,5 @@
 """ref.gd"""
-class_name Ref
+class_name RefClass
 extends Node
 
 var player : Player # Init on player _ready()
@@ -7,3 +7,14 @@ var manager : Manager # Init on manager _ready()
 var level : Level # Init on Nav _ready()
 var level_timer : LevelTimer # Init on GameTimer _ready()
 var tutorial : Tutorial
+
+enum AMMO {PISTOL, M16, SHOTGUN, SNIPER}
+
+enum ACTORS {
+	MANNEQUIN, MARKSMEN
+}
+	
+var ActorNames = {
+	ACTORS.MANNEQUIN: "Mannequin",
+	ACTORS.MARKSMEN: "Marksmen"
+}
